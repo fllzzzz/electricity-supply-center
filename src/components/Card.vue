f7504678bb3f4eb153195c4869c369bcd752185a
jxLi024 <fllzzzz01@gmail.com>
2023-12-05 17:33:42 +0800

    project plaing 20%

---
+++ src/components/Card.vue
@@ -0,0 +1,31 @@
+<style lang="scss" scoped>
+	.card {
+		width: 100%;height: 100%;
+	}
+</style>
+
+<template>
+	<div class="card">
+		<div class="item" id="header"></div>
+		<div class="item" id="body"></div>
+		<div class="item" id="footer"></div>
+	</div>
+</template>
+
+<script setup lang="ts">
+	import {
+		PropType,
+		onMounted,
+		onBeforeUnmount
+	} from 'vue';
+
+	type InboundConfig = {
+		title :string;
+	};
+
+	const props = defineProps({
+		inboundConfig: {
+			type: Object as PropType<InboundConfig>,
+		}
+	});
+</script>
