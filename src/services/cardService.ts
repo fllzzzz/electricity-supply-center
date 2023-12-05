13372c5dc0a3ed43a5257ee84c824eaff82d880a
jxLi024 <fllzzzz01@gmail.com>
2023-12-05 17:49:56 +0800

    cardService add dispose, resizeHandler

--- src/services/cardService.ts
+++ src/services/cardService.ts
@@ -1,3 +1,28 @@
 export default class CardService {
-	
+	protected static sizeOBS :ResizeObserver | undefined;
+
+	public static resizeHandler(
+		el :HTMLElement
+	) {
+		if(! this.sizeOBS) {
+			this.sizeOBS = new ResizeObserver((() => {}));
+		}
+
+		this.sizeOBS.observe(el);
+	}
+
+	public static dispose() {
+		if(! this.sizeOBS) return;
+		
+		this.sizeOBS.disconnect();
+		this.sizeOBS = undefined;
+	}
+
+	public static disposeResizeHandler(
+		el :HTMLElement
+	) {
+		if(! this.sizeOBS) return;
+
+		this.sizeOBS.unobserve(el);
+	}
 };
