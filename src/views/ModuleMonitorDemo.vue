8d46bca2a8fbe9a14bac1f3730af500fbaa2780c
jxLi024 <fllzzzz01@gmailc.om>
2023-12-14 23:32:30 +0800

    1

--- src/views/ModuleMonitorDemo.vue
+++ src/views/ModuleMonitorDemo.vue
@@ -1,9 +1,6 @@
 <style lang="scss" scoped>
-	.video-box {
-		position: absolute;
-		right: 100px;
-		top: 200px;
-		.wrapper {
+	.app-player {
+		& > .container {
 			width: vw(990);
 			height: vw(627);
 			display: flex;
@@ -16,48 +13,90 @@
 			#header {
 				height: vw(58);
 				background-image: url('@images/视频框-标题-小.png');
+				display: flex;
+				flex-flow: row nowrap;
+				justify-content: space-between;
+				align-items: center;
+				& > .box {
+					height: 100%;
+					flex-grow: 1;
+					display: flex;
+					flex-flow: row nowrap;
+					align-items: center;
+				}
+				#title {
+					justify-content: flex-start;
+					img {
+						width: 50px;
+						height: 50px;
+						object-fit: fill;
+						margin-right: 13px;
+						margin-left: 18px;
+					}
+					span {
+						font-size: 32px;
+						height: 31px;
+						font-family: Source Han Sans CN;
+						font-weight: 400;
+						color: #FFFFFF;
+						line-height: 1;
+					}
+				}
+				#options {
+					justify-content: flex-end;
+					img {
+						width: 50px;
+						height: 50px;
+						object-fit: fill;
+						margin-left: 14px;
+						&:first-child {
+							margin-left: 0;
+						}
+					}
+				}
 			}
 			#body {
 				flex: 1;
+				padding: 13px 15px 15px 15px;
 				background-image: url('@images/视频框-小.png');
+				video {
+					width: 100%;height: 100%;
+					object-fit: fill;
+				}
 			}
 		}
 	}
 </style>
 
 <template>
-	<div class="video-box">
-		<div class="wrapper">
-			<div class="item" id="header"></div>
-			<div class="item" id="body"></div>
+	<div class="app-player" ref="elPlayer">
+		<div class="container">
+			<div class="item" id="header">
+				<div class="box" id="title">
+					<img src="@images/摄像头-icon.png">
+					<span>纳斯得去的啊啊坡道爱你爹我</span>
+				</div>
+				<div class="box" id="options">
+					<img src="@images/关闭.png">
+				</div>
+			</div>
+			<div class="item" id="body">
+				<video
+					muted
+					autoplay
+					preload="none"
+					src="https://media.w3.org/2010/05/sintel/trailer.mp4" 
+				></video>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
 	import {
-		inject,
-		Ref,ref,
-		onUnmounted,
+		ref,
 		onMounted
 	} from 'vue';
 
-	const iframeReceMsg :Ref<unknown> | undefined = inject('iframeReceMsg');
-	const iframeSendMsg :Ref<unknown> | undefined = inject('iframeSendMsg');
-
-	const msg = {
-		ctid: 12911,
-		state: 'open'
-	};
-
-	iframeReceMsg && ((t, m) => t.value = m)(
-		iframeReceMsg, msg
-	);
-
-	onMounted(() => {
-		msg.state = 'close';
-		iframeReceMsg && ((t, m) => t.value = m)(
-			iframeReceMsg, msg
-		);
-	});
+	const elPlayer = ref<HTMLElement | undefined>(undefined);
 </script>
