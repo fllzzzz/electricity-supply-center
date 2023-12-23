0b942e4ee55b75d789b4d70f2f2c6b4d97e18035
jxLi024 <fllzzzz01@gmail.com>
2023-12-23 17:58:50 +0800

    232312231758

--- src/components/EchartsDemo.vue
+++ src/components/EchartsDemo.vue
@@ -13,6 +13,7 @@
 
 <script setup lang="ts">
 	import Echarts from '@/components/Echarts.vue';
+	import * as echarts from 'echarts/core';
 
 	import {
 		CustomChart,
@@ -31,7 +32,8 @@
 	} from 'echarts/renderers';
 
 	import {
-		reactive
+		reactive,
+		computed
 	} from 'vue';
 
 	import type {
@@ -55,8 +57,8 @@
 		GridComponentOption
 	} from 'echarts/components';
 
-	import {
-		type ComposeOption
+	import type {
+		ComposeOption
 	} from 'echarts/core';
 
 	type ECOption = ComposeOption<
@@ -69,138 +71,16 @@
 	>;
 
 	type Config = {
-		data :(string | number)[][],
+		data :[string, (string | number)][],
 	};
 
 	const props = defineProps({
 		config: {
-			type: Object as PropType<Config>
+			type: Object as PropType<Config>,
+			required: true,
+			default: []
 		}
 	});
-
-	const chartsDataList = reactive<Config['data']>([
-		['item1', 'item2'],
-		[2, 	10],
-		[4,		20],
-		[6,		30],
-		[8,		40],
-	]);
-
-	const options :ECOption = {
-		dataset: {
-			sourceHeader: true,
-			source: chartsDataList
-		},
-		grid: {
-			containLabel: true,
-			top: 0,
-			right: 0,
-			bottom: 0,
-			left: 0,
-		},
-		xAxis: {
-			type: 'category',
-			axisLabel: {
-				formatter(value, index) {
-					if(index === 0) return '';
-					return `${value}时`
-				},
-			},
-			axisTick: {
-				show: false
-			},
-			axisLine: {
-				show: true
-			},
-
-		},
-		yAxis: {
-			
-		},
-		series: [
-			{
-				type: 'custom',
-				coordinateSystem: 'cartesian2d',
-				encode: {
-					x: 0,
-					y: 1
-				},
-				renderItem: (params, api) => {
-					const [
-						Ox, Oy, x, y,
-					] = [
-						...api.coord([0, 0]),
-						...api.coord([api.value(0), api.value(1)])
-					];
-					const dimensionY = params.encode.y[0];
-					const diffxAxiosPoints = Render.getDiffxAxiosPoints(x);
-
-					console.log('@jx', diffxAxiosPoints);
-
-
-					const yAxisMaxValue = chartsDataList
-						.slice(1).map(row => {
-							if(dimensionY >= row.length) 
-								throw new Error('the dimension out of array length')
-						
-							return row[dimensionY];
-						}).sort((a, b) => {
-							if(
-								typeof a === 'string' ||
-								typeof b === 'string'
-							) return 0;
-
-							return a > b ? -1 : 1;
-						})[0];
-
-					if(
-						! yAxisMaxValue ||
-						typeof yAxisMaxValue !== 'number'
-					) throw new Error('yAxisMaxValue is unknown error')
-
-					const yAxisMaxPoint = api.coord([0, yAxisMaxValue]);
-
-					if(y >= yAxisMaxPoint[1] / 2) {
-						// 下方，未超过一半
-
-					}else {
-
-					}
-
-
-					return {
-						type: 'path',
-						style: {
-							stroke: 'rgba(248, 181, 81, 1)',
-							fill: 'rgba(248, 181, 81, 0.3)',
-						},
-						shape: {
-							d: `
-								M	
-									${x - 100} ${Oy}
-
-								C
-									${x - 100} ${Oy},
-									${x} ${Oy},
-									${x} ${y},
-
-								C
-									${x} ${Oy},
-									${x + 100} ${Oy},
-									${x + 100} ${Oy}
-
-								Z
-							`,
-						}
-					}
-				}
-			}
-		]
-	}
-
-	setTimeout(() => {
-		
-	}, 3000);
 
 	const useList = [
 		CustomChart,
@@ -212,26 +92,128 @@
 		LineChart,
 	];
 
+	const chartsDataList = computed(() => {
+		const result :Config['data'] = props.config?.data ?
+			props.config?.data : [
+				['item1', 'item2'],
+				['2', 	10],
+				['4',	20],
+				['6',	30],
+				['8',   30],
+			];
 
-	const Render = {
-		_x: 0,
+		result.splice(1 ,0 ,['cover1', 0]);
+		result.push(['cover2', 0]);
+
+		return result;
+	});
 
 
-		getDiffxAxiosPoints(
-			x :number
-		) {
-			let result :number;
 
-			if(this._x === 0) {
-				result = this._x;
-
-				this._x = x;
-				return result;
+	const options :ECOption = {
+		dataset: {
+			sourceHeader: true,
+			source: chartsDataList.value
+		},
+		grid: {
+			containLabel: true,
+			top: '5%',
+			right: 0,
+			bottom: 0,
+			left: 0,
+		},
+		xAxis: {
+			type: 'category',
+			axisLabel: {
+				formatter: value => {
+					if(value.slice(0, value.length - 1) !== 'cover')
+						return value;
+					return '';
+				},
+			},
+			axisTick: {
+				show: false
 			}
-			
-			result = Math.abs(this._x - x);
-			this._x = x;
-			return Math.abs(this._x - x);
-		}
-	};
+		},
+		yAxis: {},
+		series: [
+			{
+				type: 'custom',
+				encode: {
+					x: 0,
+					y: 1
+				},
+				renderItem: (params, api) => {
+					let _pathData :string = '';
+
+					if(
+						params.dataIndex >= 1 &&
+						params.dataIndex <= params.dataInsideLength - 2
+					) {
+						const [
+							Ox, Oy, x, y,
+						] = [
+							...api.coord([0, 0]),
+							...api.coord([
+								api.value(params.encode.x[0]), 
+								api.value(params.encode.y[0])
+							])
+						];
+
+						const nextXAsixPoint = 
+							api.coord(
+								[chartsDataList.value[params.dataIndex + 1][0], 0]
+							)[0];
+	
+						const xAxisDiff = Math.abs(x - nextXAsixPoint);
+								
+						_pathData = `
+							M
+								${x - xAxisDiff} ${Oy}
+							C
+								${x - (xAxisDiff / 2)} ${Oy},
+								${x} ${Oy},
+								${x} ${y}
+							C
+								${x} ${Oy},
+								${x + (xAxisDiff / 2)} ${Oy},
+								${x + xAxisDiff} ${Oy}
+							Z
+						`;
+
+						console.log(
+							'@jx', '\r\n',
+							'path: ', _pathData
+						);
+					}
+
+
+					return {
+						type: 'path',
+						style: {
+							stroke: 'rgba(248, 181, 81, 1)',
+							fill: 'none'
+/* 							fill: (() => {
+								return new echarts.graphic.LinearGradient(
+									0, 0, 0, 1, [
+										{
+											offset: 0,
+											color: 'rgba(248, 181, 81, 1)'
+										},
+										{
+											offset: 1,
+											color: 'rgba(248, 181, 81, 0.6)'
+										},
+									]
+								);
+							})(), */
+						},
+						shape: {
+							d: _pathData
+						}
+					}
+				}
+			}
+		]
+	}
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -8,7 +8,6 @@ import {
 	reactive,
 	Ref
 } from 'vue';
-
  export namespace overview {
 	export const store = reactive<{
 		model? :string;
